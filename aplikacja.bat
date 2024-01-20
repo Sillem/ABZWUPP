aee98792c7925eaf819b738f8ae9d25748fc9997
Dejwid <s205915@sggw.edu.pl>
2024-01-20 10:29:21 +0100

    instalacja bibliotek w jednym pliku ze streamlitem

--- aplikacja.bat
+++ aplikacja.bat
@@ -1,2 +1,5 @@
 @echo off
-start cmd /k "streamlit run GUI_class.py"
+py -m pip install pandas matplotlib streamlit numpy scikit-learn mplcursors plotly openpyxl bs4 kaleido
+python -m pip install pandas matplotlib streamlit numpy scikit-learn mplcursors plotly openpyxl bs4 kaleido
+streamlit run GUI_class.py
+pause
